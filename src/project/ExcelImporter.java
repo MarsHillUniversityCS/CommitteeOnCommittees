3e7256c30224298a0f1c1bf5e7c347edbd11f47c
Elijah Sears <s000192926@mhu.edu>
2019-04-02 15:21:19 -0400

    Working on inserting the csv file into the database

--- src/project/ExcelImporter.java
+++ src/project/ExcelImporter.java
@@ -1,5 +1,7 @@
 import java.io.FileReader;
 import java.io.Reader;
+import java.sql.DriverManager;
+import java.sql.Connection;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
@@ -8,10 +10,13 @@ public class ExcelImporter {
 	public static void main(String args[]){
 		new ExcelImporter().readCSV("CoC_data.csv");
 	}
-
 	
 	public void readCSV(String fn){
-		
+
+		//String windowsPath = ".\";
+
+		//Connection conn = DriverManager.getConnection(.\);
+
 		try {
 			Reader in = new FileReader(fn);
 			for(CSVRecord record : CSVFormat.RFC4180.withHeader().parse(in)) {
@@ -22,9 +27,10 @@ public class ExcelImporter {
 				System.out.println(firstName + " " + lastName + " - *" + dept + "*");
 
 				String sql = "INSERT INTO <table name> (first_name, last_name, field3...) " +
-					" VALUES ('" + firstName + "', '" + lastName + "','"; 
+					" VALUES ('" + firstName + "', '" + lastName + "','";
 
-				//create and then execute the sql statment
+
+				//create and then execute the sql statement
 	
 				//break;
 	
